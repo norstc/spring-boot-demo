10cfb2e26efb652a275f6f4d43b383ab8230a4a1
norstc <norstc@gmail.com>
2017-06-18 20:40:04 +0800

    add h2 database and h2 console

--- src/main/java/com/example/demo/system/SecurityConfiguration.java
+++ src/main/java/com/example/demo/system/SecurityConfiguration.java
@@ -8,6 +8,11 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity.authorizeRequests().antMatchers("/").permitAll();
+		httpSecurity.authorizeRequests().antMatchers("/").permitAll().and()
+		.authorizeRequests().antMatchers("/console/**").permitAll();
+		
+		//h2console setting
+		httpSecurity.csrf().disable();
+		httpSecurity.headers().frameOptions().disable();
 	}
 }
